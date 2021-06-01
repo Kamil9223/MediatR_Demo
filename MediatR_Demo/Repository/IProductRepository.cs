11e36a4322e3cf3b920e8e0c5a6afed11ec4a7c1
Kamil Mikus <kamil.mikus@mccomp.pl>
2021-06-01 15:58:14 +0200

    work in progress

---
+++ MediatR_Demo/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using Data.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProduct(int id);
+        Task<ProductWithCategory> GetProductWithCategory(int id);
+        void Add(Product product);
+    }
+}
